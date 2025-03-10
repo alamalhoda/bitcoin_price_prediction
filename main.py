b6fdda4826717b1cfbb29a58d794943b38ca0d49
salimusavi02 <s.ali.musavi02@gmail.com>
2025-03-10 03:52:13 +0330

    TCN model

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from data_handler import load_data_from_csv
 from plotting import plot_close_price, plot_volume, plot_moving_averages, plot_candlestick, plot_daily_returns
-from linear_regression import prepare_and_predict
-from random_forest import predict_with_random_forest
-from lstm_model import predict_with_lstm
-from gru_model import predict_with_gru
-from arima_model import predict_with_arima
+# from linear_regression import prepare_and_predict
+# from random_forest import predict_with_random_forest
+# from lstm_model import predict_with_lstm
+# from gru_model import predict_with_gru
+# from arima_model import predict_with_arima
 from tcn_model import predict_with_tcn
 
 
@@ -18,16 +18,16 @@ def main():
 
     # رسم نمودارها
     plot_close_price(data)
-    plot_volume(data)
+    # plot_volume(data)
     # plot_moving_averages(data)
-    plot_candlestick(data)
+    # plot_candlestick(data)
     # plot_daily_returns(data)
 
     # اجرای مدل‌ها
     # prepare_and_predict(data)
     # predict_with_random_forest(data)
     # predict_with_lstm(data)
-    predict_with_gru(data)
+    # predict_with_gru(data)
     # predict_with_arima(data)
     predict_with_tcn(data)
 
